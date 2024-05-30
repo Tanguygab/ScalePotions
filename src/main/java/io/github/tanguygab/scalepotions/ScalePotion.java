a12b5fde3051a1d8dd8de07e169465cdbe843403
Tanguygab <tanguy@gabril.com>
2024-05-30 18:55:06 +0200

    Initial commit

---
+++ src/main/java/io/github/tanguygab/scalepotions/ScalePotion.java
@@ -0,0 +1,2 @@
+package io.github.tanguygab.scalepotions;public class ScalePotion {
+}
---
+++ src/main/java/io/github/tanguygab/scalepotions/ScalePotions.java
@@ -0,0 +1,17 @@
+package io.github.tanguygab.scalepotions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ScalePotions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
